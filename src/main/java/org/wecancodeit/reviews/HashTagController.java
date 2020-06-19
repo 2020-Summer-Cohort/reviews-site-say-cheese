38068eb9ea12fde48683ef92e6f4ef4882cb444f
Rchambe9 <Rchambe9@gmail.com>
2020-06-19 11:59:15 -0400

    RC commit 6-19

--- src/main/java/org/wecancodeit/reviews/HashTagController.java
+++ src/main/java/org/wecancodeit/reviews/HashTagController.java
@@ -1,4 +1,30 @@
 package org.wecancodeit.reviews;
 
-public class HashTagController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+        public class HashTagController {
+        private HashTagRepository hashTagRepo;
+
+    public HashTagController(HashTagRepository hashTagRepo) {
+        this.hashTagRepo = hashTagRepo;
+    }
+    @GetMapping("hashTags/{id}")
+        public String showSingleHashTag(@PathVariable Long id, Model model){
+        model.addAttribute("hashTag", hashTagRepo.findById(id).get());
+        return "hashTag-template";
+    }
+    @PostMapping("hashTags/add")
+        public String addHashTag(String hashTag){
+        HashTag hashTagToAdd = new HashTag(hashTag);
+        hashTagRepo.save(hashTagToAdd);
+        return "redirect:/";
+    }
+
+
+
 }
