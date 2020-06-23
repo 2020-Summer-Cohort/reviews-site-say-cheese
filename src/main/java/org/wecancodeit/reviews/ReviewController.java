9cc04d8e2fbd347aad279e84b5f37bee4787829c
MilesInSpace <Cshank.public@gmail.com>
2020-06-23 15:13:52 -0400

    Started to add controller for creating own cheese reviews

--- src/main/java/org/wecancodeit/reviews/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewController.java
@@ -3,15 +3,20 @@ package org.wecancodeit.reviews;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
 
 @Controller 
 public class ReviewController {
 
     ReviewStorage reviewStorage;
-
+    CheeseCategoryStorage cheeseCategoryStorage;
     public ReviewController(ReviewStorage reviewStorage){
         this.reviewStorage = reviewStorage;
 
@@ -22,7 +27,23 @@ public class ReviewController {
         model.addAttribute("reviewToDisplay",reviewStorage.findReviewByCheeseName(cheeseName));
         return "review-template";
 }
-
+//      this.cheeseName = cheeseName;
+//        this.texture = texture;
+//        this.milkSource = milkSource;
+//        this.geographicLocation = geographicLocation;
+//        this.userRating = userRating;
+//        this.userReviewComment = userReviewComment;
+//        this.cheeseCategory = cheeseCategory;
+//        this.hashTags = new ArrayList<>(Arrays.asList(hashTags));
+//WE NEED TO ADAPT THIS BEFORE OUR REVIEW WORKS
+    @PostMapping("reviews/add")
+    public String addReview(String cheeseName, String texture, String milkSource, String geographicLocation, Integer userRating, String userReviewComment, String cheeseCategory, long... hashtagIds) {
+        CheeseCategory reviewCheeseCategory = cheeseCategoryStorage.findCheeseCategoryByType(CheeseType); //WIP
+//        Collection<Author> bookAuthors = Arrays.stream(authorIds)
+//                .mapToObj(id->authorStorage.findAuthorById(id))
+//                .collect(Collectors.toSet());
+//        bookStorage.save(new Book(title,isbn,description,bookCampus, bookAuthors.toArray(Author[]::new)));
+//        return "redirect:/campuses/"+campusName;
 }
 
 
